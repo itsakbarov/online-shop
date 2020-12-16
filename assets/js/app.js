df25c1779001da869aa4fae3c1e0fe6fedd5ea45
Sardor <asrorjon5538@gmail.com>
2020-12-17 00:36:16 +0500

    Initialized Carousels

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -83,7 +83,7 @@ $(document).ready(function () {
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 }
             }
